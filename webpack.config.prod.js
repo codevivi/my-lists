4260b2f0ad93f115c639958f6ec86879c12b3f05
Vilma <vilmail@protonmail.com>
2023-05-07 13:59:56 +0300

    left only index.html for deploy

--- webpack.config.prod.js
+++
@@ -1,8 +0,0 @@
-const HtmlInlineScriptPlugin = require("html-inline-script-webpack-plugin");
-const path = require("path");
-const { merge } = require("webpack-merge");
-const common = require("./webpack.config.common");
-module.exports = merge(common, {
-  mode: "none",
-  plugins: [new HtmlInlineScriptPlugin()],
-});
